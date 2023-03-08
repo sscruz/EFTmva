0675beca89c2e8c6400d0f18767a13b173c25d56
Sergio Sanchez Cruz <sergio.sanchez.cruz@cern.ch>
2023-03-08 14:53:15 +0100

    Updating example

--- examples/do_roc.py
+++ examples/do_roc.py
@@ -27,19 +27,31 @@ def main():
     likelihood = full_likelihood("/work/sesanche/EFT_mva_v2/ctg_regression.yaml")
     param_score = likelihood( features, {'ctg':2.}).detach().numpy()
     param_score = np.minimum(20,param_score)
-    sm,bins,_  = plt.hist(param_score, weights=sm_weight.detach() , bins=200 , alpha=0.5, label="SM" , density=True)
-    bsm,bins,_ = plt.hist(param_score, weights=bsm_weight.detach(), bins=bins, alpha=0.5, label="BSM", density=True)
+    parametric_sm,bins,_  = plt.hist(param_score, weights=sm_weight.detach() , bins=200 , alpha=0.5, label="SM" , density=True)
+    parametric_bsm,bins,_ = plt.hist(param_score, weights=bsm_weight.detach(), bins=bins, alpha=0.5, label="BSM", density=True)
     plt.legend()
     plt.savefig(f"{directory_name}/hist.png")
     plt.clf()
 
     dedicated = torch.load("20230308-141401_v1/network_bsm_weight_ctg_2_last.p", map_location=torch.device('cpu'))
     dedicated_score = dedicated(features).detach().numpy()
-    sm,bins,_  = plt.hist(dedicated_score, weights=sm_weight.detach() , bins=200 , alpha=0.5, label="SM" , density=True)
-    bsm,bins,_ = plt.hist(dedicated_score, weights=bsm_weight.detach(), bins=bins, alpha=0.5, label="BSM", density=True)
+    dedicated_sm,bins,_  = plt.hist(dedicated_score, weights=sm_weight.detach() , bins=200 , alpha=0.5, label="SM" , density=True)
+    dedicated_bsm,bins,_ = plt.hist(dedicated_score, weights=bsm_weight.detach(), bins=bins, alpha=0.5, label="BSM", density=True)
     plt.legend()
     plt.savefig(f"{directory_name}/hist_dedicated.png")
+    plt.clf()
+
     
+    cum_parametric_sm  = np.cumsum( parametric_sm  )
+    cum_parametric_bsm = np.cumsum( parametric_bsm )
+
+    cum_dedicated_sm  = np.cumsum( dedicated_sm  )
+    cum_dedicated_bsm = np.cumsum( dedicated_bsm )
+
+    plt.plot( cum_parametric_sm, cum_parametric_bsm, label="Parametric discriminator")
+    plt.plot( cum_dedicated_sm, cum_dedicated_bsm, label="Dedicated discriminator")
+
+
 
 if __name__=="__main__":
     main()
